e502d725e4f06026e6569a4fe7b1ee6c10d422c4
bmroby01 <brianna@eltoro.com>
2019-01-11 10:05:05 -0500

    Just corrected some spelling because I wanted to push some code to you.

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import "fmt"
 
 //Alright y'all today im gonna show you some pretty cool shit when it comes functions
 //the first thing we are gonna look at is a function definition
-//basicaly what it means is that you can define a function without actually writing what it means inside of it
-//so lets check out what that looks like
+//basically what it means is that you can define a function without actually writing what it means inside of it
+//so let's check out what that looks like
 type mystery func(a int) int
 
 //so what the fuck does mystery do?  Well don't worry about that cause I wanna show something else too
-//if you thought that was it then you'd be very in for a shock, cause yopu can actually pass a function into a function
+//if you thought that was it then you'd be very in for a shock, cause you can actually pass a function into a function
 //check this out
 func passer(a func(a int) int) {
 	fmt.Println(a(40)) //here im passing 40 into the function 'a' that was passed in
 }
 
-//shits wild, right?
+//shit's wild, right?
 
 //so what does this look like in the main?
 
@@ -28,8 +28,8 @@ func main() {
 	//why would you ever not want to define a function?
 	//well maybe you don't know all the variables you are using yet
 	//or maybe you just don't know how that'll affect your program until later
-	//or maybe you just don't kniw how yet
-	//regardless this is what we have to work wtih, so learn it
+	//or maybe you just don't know how yet
+	//regardless this is what we have to work with, so learn it
 
 	var funner mystery = func(a int) int {
 		return a + 5
@@ -46,7 +46,7 @@ func main() {
 	//and it worked, amazing right?
 
 	//the other one is a little more basic so you get the gist
-	//just make a new function kinda like bellow, asign it to a variable and pass it into the other function
+	//just make a new function kinda like below, assign it to a variable and pass it into the other function
 	passie := func(b int) int {
 		return b / 2
 	}
